0943165402b2f3397c144938c2aed40f74662d3a
yu shimamura <mashiyu0914@gmail.com>
2019-11-22 12:46:01 +0900

    update : メーラーが機能したかわかりやすく

--- app/mailers/confirm_mailer.rb
+++ app/mailers/confirm_mailer.rb
@@ -2,6 +2,6 @@ class ConfirmMailer < ApplicationMailer
   def confirm_mail(confirm)
     @confirm = confirm
 
-    mail to: @confirm.user.email, subject: "記事を作成しました"
+    mail to: @confirm.user.email, subject: @confirm.user.name + "さんの記事を作成しました"
   end
 end
